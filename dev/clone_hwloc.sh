1f8e74472cbb9d6d43b580b7531c5594ff76b048
Jiaming Yuan <jm.yuan@outlook.com>
2026-01-05 05:05:18 +0800

    string.

--- dev/clone_hwloc.sh
+++ dev/clone_hwloc.sh
@@ -9,7 +9,7 @@ fi
 
 cd /ws/
 git clone https://github.com/open-mpi/hwloc.git
-if [[ $HWLOC_MAJOR == "2" ]]; then
+if [[ "$HWLOC_MAJOR" == "2" ]]; then
     echo "Build V2"
     cd hwloc && git checkout $(cat /ws/hwloc_version_v2) && cd -
 else
