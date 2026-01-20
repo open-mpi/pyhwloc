25ad4ceaf4004696eaca18a3cf5d3d0a66b97021
Jiaming Yuan <jm.yuan@outlook.com>
2026-01-20 18:07:44 +0800

    Use toc hwloc.

--- src/pyhwloc/hwloc/__init__.py
+++ src/pyhwloc/hwloc/__init__.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2025, NVIDIA CORPORATION.
+# Copyright (c) 2025-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-__version__ = "2.0.0-dev"
+__version__ = "3.0.0-dev"
